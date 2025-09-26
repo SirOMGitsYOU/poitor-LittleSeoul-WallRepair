3f8671f4237383217936ca54d14eb06cc0864511
Peter <20475516+SirOMGitsYOU@users.noreply.github.com>
2025-09-26 19:06:33 +0100

    Upload assets + MIT license

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+games { 'gta5' }
+lua54 "yes"
+
+this_is_a_map 'yes'
+
+files {
+    'stream/ytyp/*.ytyp',
+    'stream/ydr/*.ydr',
+    'stream/ydd/*.ydd',
+    'stream/ymap/*.ymap',
+}
+
+data_file "DLC_ITYP_REQUEST" "stream/ytyp/*.ytyp"
+
+escrowignore {
+    'stream/ytyp/*.ytyp',
+    'stream/ydr/*.ydr',
+    'stream/ydd/*.ydd',
+    'stream/ymap/*.ymap',
+}
